74c51a6575d27a87f4788460f595ec602fdd8b74
Stand <99733463+StandVideo@users.noreply.github.com>
2026-08-31 14:59:52 +0200

    Update ps4_offsets.js

--- ps4_offsets.js
+++ ps4_offsets.js
@@ -471,6 +471,15 @@ PS4["13.52"] = Object.assign({}, PS4["13.00"], {
     kpatch: "1300.bin",
 });
 
+PS4["13.02"] = Object.assign({}, PS4["13.00"], {
+    alias_of: "13.00",
+    fw_status: "state=UNTESTED-on-hardware shares=13.00 "
+        + "webkit=assumed-identical-to-13.00 "
+        + "kernel_rvas=untested-vs-dump "
+        + "kpatch=1300.bin",
+    kpatch: "1300.bin",
+});
+
 export function offsetsFor(uaString) {
     const m = (uaString || "").match(/PlayStation\s+4[\/ ](\d+)\.(\d+)/);
     if (!m) return { key: null, off: null };
